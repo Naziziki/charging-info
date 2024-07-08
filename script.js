283dd392172dc8a297ed4506c07acc1fddbfb270
Naziziki <nazarharin.nh@gmail.com>
2024-07-08 22:30:36 +0300

    New project

---
+++ script.js
@@ -0,0 +1,44 @@
+initBattery();
+console.log(navigator.getBattery());
+function initBattery() {
+    const bPercentage = document.getElementById('percentage');
+    const bStatus = document.getElementById('levelPercentage');
+    const bDetails = document.getElementById('info-details');
+
+    navigator.getBattery().then((batt) => {
+        let updateBattery = () => {
+            bPercentage.innerHTML = `${batt.level * 100}%`;
+            let level = batt.level * 100;
+            console.log(level); 
+            bStatus.style.height = `${level}%`;
+            if (level <= 20) {
+                bStatus.style.background = 'var(--gradient-color-red)';
+            } else if (level <= 48) {
+                bStatus.style.background = 'var(--gradient-color-orange)';
+            } else if (level <= 80) {
+                bStatus.style.background = 'var(--gradient-color-yellow)';
+            } else {
+                bStatus.style.background = 'var(--gradient-color-green)';
+            }
+
+            if (batt.charging == true) {
+                bDetails.innerHTML = `Charging <i class="fa-solid fa-bolt animated-green"></i>`;
+            } else if (level <= 20 && batt.charging == false) {
+                bDetails.innerHTML = `Low battery \n Please connect the charger <i class="fa-solid fa-plug animated-red"></i>`;
+            } else if (level == 100) {
+                bDetails.innerHTML = `Fully charged <i class="fa-solid fa-bolt animated-green">`;
+            } else {
+                bDetails.innerHTML = '';
+            }
+
+
+
+
+            batt.addEventListener("levelchange", () => { updateBattery(); });
+            batt.addEventListener("chargingchange", () => { updateBattery(); });
+            // console.log(bStatus.style.height);
+
+        }
+        updateBattery();
+    });
+};
